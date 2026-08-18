265ed1c33d54c944c6c2f8ebb8fe62c44f772bf6
Vinze Lianne <cinco.vinzelianne@llcc.edu.ph>
2026-08-18 22:25:20 +0800

    remove use Throwable;

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
